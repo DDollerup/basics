2818d1892ada39e7934a137e32ba2c8f971ae49f
Daniel Dollerup <d.dollerup@gmail.com>
2023-10-05 12:54:56 +0200

    Changed to black theme

--- src/components/layout/nav/Nav.js
+++ src/components/layout/nav/Nav.js
@@ -19,7 +19,7 @@ const Nav = () => {
                                 Dropdown
                             </a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">A</a></li>
                                 <li><a className="dropdown-item" href="#">Another action</a></li>
                                 <li><hr className="dropdown-divider" /></li>
                                 <li><a className="dropdown-item" href="#">Something else here</a></li>
