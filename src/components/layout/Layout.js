8323d7c81b6fdb832a1fec79b18762ed1d299a4d
Daniel Dollerup <d.dollerup@gmail.com>
2023-10-05 12:22:34 +0200

    Change to Layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -7,6 +7,7 @@ const Layout = ({ children }) => {
         <>
             <Nav />
             <Header />
+            Hello
             <main className="container-fluid my-3">
                 {children}
             </main>
